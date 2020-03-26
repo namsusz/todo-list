940465012f21d57574bcb5b7d1732fad938092b7
namsusz <61674318+namsusz@users.noreply.github.com>
2020-03-26 14:57:57 +0900

    Updates

---
+++ precache-manifest.57ed06f52f5c0b7c57530682eef62c87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f1d638528f565ed63478bc91f4cffbc",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "f2cf1fe67f2a2db7a347",
+    "url": "/todo-list/static/css/main.093c9111.chunk.css"
+  },
+  {
+    "revision": "307227dbbc043e3fe105",
+    "url": "/todo-list/static/js/2.3b50ce1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.3b50ce1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2cf1fe67f2a2db7a347",
+    "url": "/todo-list/static/js/main.1038c1ba.chunk.js"
+  },
+  {
+    "revision": "4c207b602d40809001a5",
+    "url": "/todo-list/static/js/runtime-main.2929926a.js"
+  }
+]);
